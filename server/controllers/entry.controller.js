e9d521ad23c3e2de2fa1654138b405df68769588
Fig Yogurt <itisaftab@gmail.com>
2021-02-16 16:33:38 -0500

    feat(entry): postNewEntry now stores the sentences model with it its relation attached

--- server/controllers/entry.controller.js
+++ server/controllers/entry.controller.js
@@ -1,11 +1,13 @@
-const { db, User, EntryTone, SentenceTone } = require("../db");
+const { EntryTone, SentenceTone } = require("../db");
 
 exports.postNewEntry = async (req, res) => {
   try {
-    const { document_tone, sentence_tone } = req.entryResult;
+    const { document_tone, sentences_tone } = req.entryResult;
     const entryTonesList = document_tone.tone_categories;
+    const sentenceToneList = sentences_tone;
 
     const diaryPost = {};
+
     //loop to get emotions for the entry model
     for (let i = 0; i < entryTonesList.length - 1; i++) {
       const emotionCategory = entryTonesList[i].tones;
@@ -13,12 +15,37 @@ exports.postNewEntry = async (req, res) => {
         (emotion) => (diaryPost[emotion.tone_id] = emotion.score)
       );
     }
+
     diaryPost.userId = req.userId;
     diaryPost.message = req.body.message;
-    const newestEntry = await EntryTone.create(diaryPost);
-    res.json({ what: req.entryResult });
+    const newestDiaryEntry = await EntryTone.create(diaryPost);
+
+    // loop to get all the emotions for each sentence
+    for (let i = 0; i < sentenceToneList.length; i++) {
+      const sentenceEl = sentenceToneList[i];
+      const oneSenPost = {};
+
+      const senToneCategories = sentenceEl.tone_categories;
+      for (let i = 0; i < senToneCategories.length - 1; i++) {
+        const emotionCategory = senToneCategories[i].tones;
+        emotionCategory.map(
+          (emotion) => (oneSenPost[emotion.tone_id] = emotion.score)
+        );
+      }
+
+      oneSenPost.message = sentenceEl.text;
+      oneSenPost.userId = req.userId;
+      oneSenPost.entryToneId = newestDiaryEntry.id;
+
+      const newestSentenceEntry = await SentenceTone.create(oneSenPost);
+    }
+
+    res.json({
+      message: "Your diary entry has been saved!",
+      data: newestDiaryEntry.dataValues,
+    });
   } catch (error) {
     console.log(error);
-    return res.json({ message: "Error occured during new Entry post" });
+    return res.json({ message: "Error occured when saving new Entry post" });
   }
 };
